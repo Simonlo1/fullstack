8042c823bb49c737eb38b684d4ea37ad7a9d1881
unknown <lawyer70130@gmail.com>
2018-10-30 00:51:45 -0500

    first draft

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,6 +5,39 @@ import { Component } from '@angular/core';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
 export class AppComponent {
-  title = 'Fullstack';
+    /*title = 'Fullstack';
+    counter = 0;*/
+    spices: string[] = ['蒜蓉', '椒盐', '姜葱'];
+    food: string[] = ['龙虾', '生蚝', '牛肉'];
+    goodResult = false;
+    isLogin = false;
+    userName: string;
+    /*counterPlus() {
+        this.counter ++;
+    }
+
+    resetCounter() {
+        this.counter = 0;
+    }*/
+
+    isResultGood() {
+        this.goodResult = true;
+        /*return this.goodResult;*/
+    }
+
+    login() {
+        this.isLogin = true;
+    }
+
+    signOut() {
+        this.isLogin = false;
+    }
+
+    // Event Binding
+    /*onUpdateUserName(event: Event) {
+        this.userName = (<HTMLInputElement>event.target).value;
+    }*/
+
 }
